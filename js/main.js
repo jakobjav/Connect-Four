7e80b9109177ec2bcd5c4b1b87bfe44600d13601
jakobjav <jakob.vallesteros@gmail.com>
2022-10-17 13:13:07 -1000

    stackable tokens still need win cond

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
 //Variables
-let boardGame = document.querySelector(".board")
-let player = document.querySelector(".player")
-let playAgain = document.querySelector(".playAgain")
-let reset = document.querySelector(".reset")
-let winningArray = [[]]
 
-let board;
-let rows = 6;
-let columns = 7;
+let gameOver = false
+let board
+let currentColumns
+
+let rows = 6
+let columns = 7
+
+let playerRed = "R"
+let playerYellow = "Y"
+let currentPlayer = playerRed
+
+//Functions
 
 //calling gameStart will call upon other functions that will create gameboard, set players, reset score, etc
 gameStart()
@@ -18,38 +22,60 @@ function gameStart(){
 
 //function below will create board and 49 white cells
 function createBoard() {
-    board = [];
+    board = []
+    currentColumns = [5, 5, 5, 5, 5, 5, 5]
 
     for (let r = 0; r < rows; r++) {
-        let row = [];
+        let row = []
         for (let c = 0; c < columns; c++) {
-            row.push(' ');
+            row.push(' ')
 
-            let cell = document.createElement("div");
+            let cell = document.createElement("div")
             cell.id = r.toString() + "-" + c.toString()
-            cell.classList.add("cell");
-            document.getElementById("board").append(cell);
+            cell.classList.add("cell")
+            cell.addEventListener('click', dropToken)
+            document.getElementById("board").append(cell)
         }
-        board.push(row);
+        board.push(row)
     }
 }
 
-function switchTurn(turn) {
+function dropToken() {
+    if (gameOver){
+        return;
+    }
+
+    let coords = this.id.split("-") // converts string '0-0' into array ['0', '0']
+    let r = parseInt(coords[0])
+    let c = parseInt(coords[1])
+
+    r = currentColumns[c];
+    if (r < 0){
+        return;
+    }
+
+    board[r][c] = currentPlayer
+    let cell = document.getElementById(r.toString() + "-" + c.toString())
+    if (currentPlayer == playerRed) {
+        cell.classList.add('red-Token')
+        currentPlayer = playerYellow
+    } else {
+        cell.classList.add('yellow-Token')
+        currentPlayer = playerRed
+    }
+    
+    r -= 1; //moves token up one row per click
+    currentColumns[c] = r; //updates array input
+}
+
+function checkWinner() {
 
 }
 
-function dropToken(token) {
+function congratsMsg() {
 
 }
 
-function checkWinner(winner) {
-
-}
-
-function congratsMsg(msg) {
-
-}
-
-function gameReset(reset) {
+function gameReset() {
 
 }
